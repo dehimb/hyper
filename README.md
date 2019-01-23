680a2aef200c2f5a0219dcb46289841a16928340
dehimb <dehimb@gmail.com>
2019-01-23 13:09:02 +0200

    Created install script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hyper
 My hyper terminal config
+sh -c "$(wget -O - https://raw.githubusercontent.com/dehimb/hyper/master/install.sh -O -)"
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -R -f ~/.hyper
+rm -f ~/.hyper.js
+mkdir ~/.hyper
+git clone https://github.com/dehimb/hyper.git ~/.hyper
+ln -sf ~/.hyper/.hyper.js ~/.hyper.js
